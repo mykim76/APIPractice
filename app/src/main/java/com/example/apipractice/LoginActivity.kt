4317997e4267d17e242ea8cb7ea59be0bedde801
mykim76 <iyoungfm@gmail.com>
2020-06-15 19:54:22 +0900

    로그링ㄴ 버튼이 눌리면 이메일 / 비밀번호 받아오기 // 서버요청 준비중

--- app/src/main/java/com/example/apipractice/LoginActivity.kt
+++ app/src/main/java/com/example/apipractice/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.example.apipractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
     override fun setValues() {
+        btnLogin.setOnClickListener {
+
+
+        }
 
     }
 
@@ -14,7 +18,7 @@ class LoginActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
 
         setValues()
         setupEvents()
