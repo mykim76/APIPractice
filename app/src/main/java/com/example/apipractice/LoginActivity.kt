ae585a7a2f06a9a5830f5eb8574fb6dce071814f
mykim76 <iyoungfm@gmail.com>
2020-06-15 20:03:17 +0900

    서버전담하는 통신 클래스 생성 =>okhttp 라이브러리 설치 + 인터넷 사용할 수 있도록 권한 추가(Manifest.xml)

--- app/src/main/java/com/example/apipractice/LoginActivity.kt
+++ app/src/main/java/com/example/apipractice/LoginActivity.kt
@@ -6,8 +6,9 @@ import kotlinx.android.synthetic.main.activity_login.*
 class LoginActivity : BaseActivity() {
     override fun setValues() {
         btnLogin.setOnClickListener {
-
-
+            val email = emailEdt.text.toString()
+            val password = passwordEdt.text.toString()
+            //서버에서 로그인 처리 불러오기
         }
 
     }
---
+++ app/src/main/java/com/example/apipractice/utils/ServerUtil.kt
@@ -0,0 +1,4 @@
+package com.example.apipractice.utils
+
+class ServerUtil {
+}
