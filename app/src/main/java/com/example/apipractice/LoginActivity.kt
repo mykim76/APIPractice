a4ebe4904e537d96521f2e5a637e8dccdfa60ddc
mykim76 <iyoungfm@gmail.com>
2020-06-15 20:59:47 +0900

    로그인 화면에서 실제 로그인 API 호출 => 응답 내용은 로그로 확인중

--- app/src/main/java/com/example/apipractice/LoginActivity.kt
+++ app/src/main/java/com/example/apipractice/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.apipractice
 
 import android.os.Bundle
+import com.example.apipractice.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : BaseActivity() {
@@ -9,6 +10,7 @@ class LoginActivity : BaseActivity() {
             val email = emailEdt.text.toString()
             val password = passwordEdt.text.toString()
             //서버에서 로그인 처리 불러오기
+            ServerUtil.postRequestLogin(mContext,email,password,null)
         }
 
     }
