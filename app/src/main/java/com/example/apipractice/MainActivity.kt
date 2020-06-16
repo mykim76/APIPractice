15cc74e98ce051719a4c8b581d9114cbb115c8d4
mykim76 <iyoungfm@gmail.com>
2020-06-16 22:20:17 +0900

    리스트뷰에 사용할 재료로 파싱한 토픽클래스 대입

--- app/src/main/java/com/example/apipractice/MainActivity.kt
+++ app/src/main/java/com/example/apipractice/MainActivity.kt
@@ -9,6 +9,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val topicList = ArrayList<Topic>()
+
     override fun setValues() {
 
         // 진행중인 토론 목록이 어떤게 있는지 무어보자
@@ -24,6 +27,7 @@ class MainActivity : BaseActivity() {
                 {
                     val topicJson = topics.getJSONObject(i)
                     val topic = Topic.getTopicFromJson(topicJson)//주제 하나에 대응되는 JSON을 넣어서 Topic 객체로 얻어내자
+                    topicList.add(topic)//주제목록을 리스트뷰 재료로 추가
 
                 }
 
