06f446d6e899d430f7451ec9553ea3e332884a72
mykim76 <iyoungfm@gmail.com>
2020-06-16 20:06:49 +0900

    회원가입 호출전 클라이언트 자제 검증(이멜, 닉넴,비번 체크)

--- app/src/main/java/com/example/apipractice/SingUpActivity.kt
+++ app/src/main/java/com/example/apipractice/SingUpActivity.kt
@@ -2,11 +2,17 @@ package com.example.apipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.apipractice.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sing_up.*
 import org.json.JSONObject
 
 class SingUpActivity : BaseActivity() {
+
+    var isEmailOK = false;
+    var isNickOK = false;
+    var isPWOK = false;
+
     override fun setValues() {
 
     }
@@ -23,6 +29,7 @@ class SingUpActivity : BaseActivity() {
                     runOnUiThread {
                         if (code == 200) {
                             txtCheckEmailResult.text = "사용해도 좋음"
+                            isEmailOK = true
                         } else {
                             txtCheckEmailResult.text = "중복 아이디임"
 
@@ -47,6 +54,7 @@ class SingUpActivity : BaseActivity() {
                         if(code == 200)
                         {
                             txtCheckNickResult.text="사용해도 좋음"
+                            isNickOK = true;
                         }
                         else{
                             
@@ -57,6 +65,31 @@ class SingUpActivity : BaseActivity() {
 
 
             })
+        }
+        
+        btnSignUp.setOnClickListener { 
+            
+            //회원가입API를 호출하기 전 자체 검사
+            //1) 이메일 2) 닉네임 중복검사 3) 비번은 8글자 이상이어야 함 => 틀린 경우가 발견되는 케이스와 내용을 토스트로 띄우기
+            if(!isEmailOK){
+                Toast.makeText(mContext,"이메일 중복검사를 통과해야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(!isNickOK){
+                Toast.makeText(mContext,"닉네임 중복검사를 통과해야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val pw = etdPW.text.toString()
+            if(pw.length>8) {isPWOK = true}
+
+            if(!isPWOK){
+                Toast.makeText(mContext,"비밀번호는 8자 이상이어야 합니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+
         }
     }
 
