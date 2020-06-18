2980647548e4e22fe569cf83e48579dc1f97b62d
mykim76 <iyoungfm@gmail.com>
2020-06-18 22:18:43 +0900

    게시글 작성시간 시차 반영 => 영국시간(세계표준시)/내폰의 설정 시간대의 시차를 계산해서 변경

--- app/src/main/java/com/example/apipractice/datas/TopicReply.kt
+++ app/src/main/java/com/example/apipractice/datas/TopicReply.kt
@@ -35,6 +35,11 @@ class TopicReply {
             
             val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")//string 분석할 양식을 클래스로 생성
             tr.createdAt.time= sdf.parse(createdAtStr)!!//파싱중인 의견 작성시간을 서버에서 알려준 작성시간으로 대입
+            
+            val myPhoneTimeZone = tr.createdAt.timeZone //어느지역 시간대인지 따서 저장
+
+            val timeOffset = myPhoneTimeZone.rawOffset / 1000/60/60 //몇시간 차이가 나는지 계산 rawOffset:밀리초단위 =>시간으로 변경
+            tr.createdAt.add(Calendar.HOUR, timeOffset) //계시글 작성시간을 timeOffset만큼 변경
 
             return tr
         }
