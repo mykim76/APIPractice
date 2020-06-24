64c00d38c53381a741fa67258712bc0c303e66bf
mykim76 <iyoungfm@gmail.com>
2020-06-24 19:53:45 +0900

    스플래시를 제외한 모든 화면에 액션바 적용

--- app/src/main/java/com/example/apipractice/BaseActivity.kt
+++ app/src/main/java/com/example/apipractice/BaseActivity.kt
@@ -1,10 +1,34 @@
 package com.example.apipractice
 
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity : AppCompatActivity() {
 
     val mContext = this
     abstract fun setValues()
     abstract fun setupEvents()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.let {
+            setCustomActionBar() //BaseActivity를 상속받는 모든 액티비티는 자동으로 이 함수 실행
+        }
+
+    }
+    fun  setCustomActionBar(){ //액션바 관련 세팅 변경
+
+        // 액션바 커스텀 기능 활성화
+        //supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.custom_action_bar)
+
+        //커스텀 액션바 영역 확장 => 윗단 여백 제거
+        supportActionBar?.setBackgroundDrawable(null) //기본 배경색 제거
+        val parent = supportActionBar?.customView?.parent as Toolbar //실제 여백 제거
+        parent.setContentInsetsAbsolute(0,0)
+    }
 }
--- app/src/main/java/com/example/apipractice/MainActivity.kt
+++ app/src/main/java/com/example/apipractice/MainActivity.kt
@@ -112,19 +112,7 @@ class MainActivity : BaseActivity() {
 
         setValues()
         setupEvents()
-        setCustomActionBar()
     }
     
-    fun  setCustomActionBar(){ //액션바 관련 세팅 변경
 
-        // 액션바 커스텀 기능 활성화
-        //supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
-        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-        supportActionBar?.setCustomView(R.layout.custom_action_bar)
-
-        //커스텀 액션바 영역 확장 => 윗단 여백 제거
-        supportActionBar?.setBackgroundDrawable(null) //기본 배경색 제거
-        val parent = supportActionBar?.customView?.parent as Toolbar //실제 여백 제거
-        parent.setContentInsetsAbsolute(0,0)
-    }
 }
