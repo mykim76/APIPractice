07c30220f99d31a98d3a717f93b70e4391597488
mykim76 <iyoungfm@gmail.com>
2020-06-25 21:07:04 +0900

    수신 이벤트 처리용 클래스 생성, 메니페스트에 등록

--- app/src/main/java/com/example/apipractice/fcm/MyFirebaseService.kt
+++ app/src/main/java/com/example/apipractice/fcm/MyFirebaseService.kt
@@ -3,6 +3,7 @@ package com.example.apipractice.fcm
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseService : FirebaseMessagingService() {
 
@@ -10,5 +11,11 @@ class MyFirebaseService : FirebaseMessagingService() {
         super.onNewToken(p0)
 
         Log.d("새 토큰 발급",p0)
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) { //실제 푸시알림 수신시 실행되는 함수
+        super.onMessageReceived(p0)
+
+        Log.d("푸쉬알림 수신",p0.notification?.title)
     }
 }
